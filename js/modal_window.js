ded04cdc388805f58cfa303c9fbe1b95e7b15615
Temyper <pall47g1@gmail.com>
2021-02-05 14:44:05 +0900

    [Add] changing text by clicking in the modal window.

--- js/modal_window.js
+++ js/modal_window.js
@@ -1,13 +1,22 @@
-// TODO:20210204入力フォームとその反映
+// TODO:20210204入力フォームとその反映（ttps://daib-log.com/modalwindow/）
 $(function () {
   $("#button-modal-window").click(function (e) {
     $("#modal-window").fadeIn();
     e.preventDefault();
   });
 
-  $("#modal-window__close, #modal-window__wrapper").click(function (e) {
-    $("#modal-window").fadeOut();
+  $("#modal-window__button-answer").click(function (e) {
+    $("#answer").text("answer");
+    console.log("write");
+    // 20210205別々に付与されたイベントは妨げられない
+    e.preventDefault();
+  });
 
+  $(
+    "#modal-window__close, #modal-window__wrapper, #modal-window__button-answer"
+  ).click(function (e) {
+    $("#modal-window").fadeOut();
+    console.log("close");
     e.preventDefault();
   });
 });
